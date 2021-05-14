3c079ba3962b21d65fc69e729ae750beb2162425
Corrie Grimshaw <corrie@pcg.local>
2021-05-14 13:42:08 -0700

    Refactor: Change cat fact API used, add final touch ups to design.

--- script.js
+++ script.js
@@ -12,31 +12,43 @@ function onClick(e){
 function animateCurtains(){
   const left = document.getElementById("left")
   const right = document.getElementById("right")  
+  
+  //only for initial
+  if (left.classList.contains("left")){
+    left.classList.remove("left");
+    right.classList.remove("right");
 
-  left.classList.toggle("left-close");
-  right.classList.toggle("right-close");
+    left.classList.add("left-open");
+    right.classList.add("right-open");
 
-  left.classList.toggle("left-open");
-  right.classList.toggle("right-open");  
+  } else {
+
+    left.classList.toggle("left-close");
+    right.classList.toggle("right-close");
+
+    left.classList.toggle("left-open");
+    right.classList.toggle("right-open");  
+  }
 }
 
 function updateCatFact(){
    const factContainer = document.getElementById("fact");
    Promise.all([
-     fetch("https://cat-fact.herokuapp.com/facts/random?amount=1").then(value => value.json()),
+     //fetch("http://cat-fact.herokuapp.com/facts/random?amount=1").then(value => value.json()),
+     fetch("https://catfact.ninja/fact?max-length=140").then(value => value.json()),
      fetch("https://cataas.com/cat/gif?json=true").then(value => value.json())
     ])
     .then(values => {
-       if (!values[0]["status"].verified) {
-          updateCatFact();  
-       } else {
+       //if (!values[0]["status"].verified) {
+       //   updateCatFact();  
+       //} else {
           if (!document.getElementById('left').classList.contains("left-open")){
-            factContainer.innerHTML = values[0]["text"];
+            factContainer.innerHTML = values[0]["fact"];
             document.getElementById('center').style.backgroundImage = 'url("' + "https://cataas.com/" + values[1]["url"] + '")';
           }
-         animateCurtains();
-       }
-     }) 
+          animateCurtains();
+       //}
+     }).catch(err => console.log(err)); 
 }
 
 
